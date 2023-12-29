a45edbd86d01e3ad661443bc191c79f587a2d2aa
Jason Chiang <86921338+chij6@users.noreply.github.com>
2023-12-29 16:52:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ def handle_message(event):
         GPT_answer = GPT_response(msg)
         print(GPT_answer)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
+        print("Responded by the GPT-3.5-Turbo AI robot. [Warning : AI responses cannot replace any professional medical advice or diagnosis.]")
     except:
         print(traceback.format_exc())
         line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
